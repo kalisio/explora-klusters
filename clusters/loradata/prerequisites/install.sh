c073947c65fb8db9682f904f901d7f3a134ce396
Robin Bourianes <robin.bourianes@kalisio.com>
2024-04-11 11:10:00 +0200

    Initial commit

---
+++ clusters/loradata/prerequisites/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_PATH=$(dirname "$THIS_FILE")
+CLUSTER_PATH=$(dirname "$THIS_PATH")
+
+export KUBECONFIG=$CLUSTER_PATH/kubeconfig.dec.yml
+
+# Key file containing identities used to decrypt secrets
+# This is used by sops, which is also used by helm-secrets plugin
+export SOPS_AGE_KEY_FILE=$DEVELOPMENT_DIR/age/keys.txt
+
+KUBECONFIG_ENC=$(dirname "$KUBECONFIG")/$(basename "$KUBECONFIG" .yaml).enc.yaml
+sops --decrypt --output "$KUBECONFIG" "$KUBECONFIG_ENC"
+
+cd "$THIS_PATH" && helmfile --concurrency 4 sync
