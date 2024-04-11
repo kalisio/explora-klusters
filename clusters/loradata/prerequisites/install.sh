9f97ba05865cc49d0b590f2a3c1a3720cb43c98d
Robin Bourianes <robin.bourianes@kalisio.com>
2024-04-11 16:34:52 +0200

    Added Romain to sops recipients

--- clusters/loradata/prerequisites/install.sh
+++ clusters/loradata/prerequisites/install.sh
@@ -5,13 +5,13 @@ THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
 THIS_PATH=$(dirname "$THIS_FILE")
 CLUSTER_PATH=$(dirname "$THIS_PATH")
 
-export KUBECONFIG="$CLUSTER_PATH/kubeconfig.dec.yml"
+export KUBECONFIG="$CLUSTER_PATH/kubeconfig.dec.yaml"
 
 # Key file containing identities used to decrypt secrets
 # This is used by sops, which is also used by helm-secrets plugin
 export SOPS_AGE_KEY_FILE="$DEVELOPMENT_DIR/age/keys.txt"
 
-KUBECONFIG_ENC=$(dirname "$KUBECONFIG")/$(basename "$KUBECONFIG" .dec.yml).enc.yml
+KUBECONFIG_ENC=$(dirname "$KUBECONFIG")/$(basename "$KUBECONFIG" .dec.yaml).enc.yaml
 sops --decrypt --output "$KUBECONFIG" "$KUBECONFIG_ENC"
 
 cd "$THIS_PATH" && helmfile --concurrency 4 sync
