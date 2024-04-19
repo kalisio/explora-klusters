13ce20edb92c6b45d38f5e0a638178ecd12d5cd5
Romain JACQUET <romain.jacquet.dev@free.fr>
2024-04-19 10:33:22 +0200

    migrate to the last version of chirpstack chart: 0.0.12 add options to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,18 @@ set -euxo pipefail
 
 NAMESPACE=${1}
 
+
+if [ $# -ge 2 ];
+then
+    shift
+    ACTION="$1"
+    shift
+    ARGS="$@"
+else
+    echo "Usage $0 namespace helmfileaction [args]"
+    echo "$0 loradata diff"
+fi
+
 THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
 THIS_DIR=$(dirname "$THIS_FILE")
 ROOT_DIR=$(dirname "$THIS_DIR")
@@ -14,5 +26,5 @@ decrypt_secrets "$NAMESPACE"
 
 # And run helmfile with proper options
 cd "$ROOT_DIR/namespaces/$NAMESPACE"
-helmfile --namespace "$NAMESPACE" --selector action=config --selector action=install --concurrency 4 sync
+helmfile --namespace "$NAMESPACE" --selector action=config --selector action=install --concurrency 4 "$ACTION"
 cd ~-
